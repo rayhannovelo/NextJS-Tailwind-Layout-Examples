120cff978afe891f55e75603d7ed3fba7038df6c
Muhammad Rayhan Novelo <rayhan.novelo@gmail.com>
2024-06-21 14:48:23 +0700

    add display example

---
+++ app/normal-flow/page.tsx
@@ -0,0 +1,24 @@
+import "./styles.css";
+
+export default function Home() {
+  return (
+    <div>
+      <a href="/" className="underline mb-3 block">
+        Back To Menu
+      </a>
+      <h1>Normal Flow</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam,
+        inventore eius in officiis vitae velit maxime aliquid nihil est
+        similique ad magnam fuga neque! Dicta iste sit rerum dolore minus?
+      </p>
+      <h2>Normal Flow</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi
+        nihil reiciendis possimus expedita nesciunt mollitia eveniet, sapiente
+        dolor, perspiciatis est sunt, voluptates dolorum ducimus impedit ratione
+        sint placeat? Possimus, tempore.
+      </p>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
+import Link from "next/link";
+
 export default function Home() {
-  return (<div>NextJS Tailwind Training</div>);
+  return (
+    <div>
+      <h1 className="text-center">Tutorial Tailwind Layout</h1>
+      <div className="flex justify-center">
+        <ul className="flex space-x-4">
+          <li>
+            <Link href="/normal-flow">Normal Flow</Link>
+          </li>
+          <li>Display</li>
+          <li>FlexBox</li>
+          <li>Grid</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
