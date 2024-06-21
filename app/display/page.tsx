025c2ad682a8fd1cf8c00f5ae08ae51a248d4729
Muhammad Rayhan Novelo <rayhan.novelo@gmail.com>
2024-06-21 16:56:13 +0700

    add grid block and inline-block example

---
+++ app/display/page.tsx
@@ -0,0 +1,71 @@
+import Link from "next/link";
+import "./styles.css";
+
+export default function Page() {
+  return (
+    <div>
+      <Link href="/" className="underline mb-3 block">
+        Back To Menu
+      </Link>
+      <h3 className="ml-1">display: inline-block, width: 400px</h3>
+      <div className="content inline-block bg-blue-400 m-1 p-3">
+        <h1>Konten 1</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci in,
+          est magnam eos distinctio officiis eligendi odit maxime laudantium,
+          dolorem voluptate inventore blanditiis, earum veniam ex harum optio
+          nostrum quis.
+        </p>
+      </div>
+      <div className="content inline-block bg-emerald-400 m-1 p-3">
+        <h1>Konten 2</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci in,
+          est magnam eos distinctio officiis eligendi odit maxime laudantium,
+          dolorem voluptate inventore blanditiis, earum veniam ex harum optio
+          nostrum quis.
+        </p>
+      </div>
+      <div className="content inline-block bg-yellow-400 m-1 p-3">
+        <h1>Konten 3</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci in,
+          est magnam eos distinctio officiis eligendi odit maxime laudantium,
+          dolorem voluptate inventore blanditiis, earum veniam ex harum optio
+          nostrum quis.
+        </p>
+      </div>
+
+      <hr className="block my-5"></hr>
+
+      <h3 className="ml-1">display: block, width: 400px</h3>
+      <div className="content block bg-blue-400 m-1 p-3">
+        <h1>Konten 1</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci in,
+          est magnam eos distinctio officiis eligendi odit maxime laudantium,
+          dolorem voluptate inventore blanditiis, earum veniam ex harum optio
+          nostrum quis.
+        </p>
+      </div>
+      <div className="content block bg-emerald-400 m-1 p-3">
+        <h1>Konten 2</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci in,
+          est magnam eos distinctio officiis eligendi odit maxime laudantium,
+          dolorem voluptate inventore blanditiis, earum veniam ex harum optio
+          nostrum quis.
+        </p>
+      </div>
+      <div className="content block bg-yellow-400 m-1 p-3">
+        <h1>Konten 3</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci in,
+          est magnam eos distinctio officiis eligendi odit maxime laudantium,
+          dolorem voluptate inventore blanditiis, earum veniam ex harum optio
+          nostrum quis.
+        </p>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,30 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-center">Tutorial Tailwind Layout</h1>
+      <h1 className="text-center my-3">Tutorial Tailwind Layout</h1>
       <div className="flex justify-center">
-        <ul className="flex space-x-4">
+        <ol className="list-decimal flex space-x-12">
           <li>
-            <Link href="/normal-flow">Normal Flow</Link>
+            <Link href="/normal-flow" className="hover:underline">
+              Normal Flow
+            </Link>
           </li>
-          <li>Display</li>
-          <li>FlexBox</li>
-          <li>Grid</li>
-        </ul>
+          <li>
+            <Link href="/display" className="hover:underline">
+              Display
+            </Link>
+          </li>
+          <li>
+            <Link href="/flex" className="hover:underline">
+              Flex
+            </Link>
+          </li>
+          <li>
+            <Link href="/grid" className="hover:underline">
+              Grid
+            </Link>
+          </li>
+        </ol>
       </div>
     </div>
   );
