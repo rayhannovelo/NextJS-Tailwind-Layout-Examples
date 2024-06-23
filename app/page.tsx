77fc667ea80723b6edf8c3792608d8ac91b2fce9
Muhammad Rayhan Novelo <rayhan.novelo@gmail.com>
2024-06-23 19:51:34 +0700

    optimize homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,37 +2,40 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-center my-3">Tutorial Tailwind Layout</h1>
-      <div className="flex justify-center">
-        <ol className="list-decimal flex space-x-12">
-          <li>
-            <Link href="/normal-flow" className="hover:underline">
-              Normal Flow
-            </Link>
-          </li>
-          <li>
-            <Link href="/display" className="hover:underline">
-              display inline block
-            </Link>
-          </li>
-          <li>
-            <Link href="/flex-direction-wrap-order" className="hover:underline">
-              flex-direction, flex-wrap, order
-            </Link>
-          </li>
-          <li>
-            <Link href="/flex-grow-shrink" className="hover:underline">
-              flex-grow, flex-shrink, flex-basis
-            </Link>
-          </li>
-          <li>
-            <Link href="/grid" className="hover:underline">
-              Grid
-            </Link>
-          </li>
-        </ol>
-      </div>
+    <div className="min-h-screen bg-cyan-400 flex flex-col justify-center items-center">
+      <h1 className="text-center py-3">Tutorial Tailwind Layout</h1>
+      <ol className="list-decimal">
+        <li>
+          <Link href="/normal-flow" className="hover:underline">
+            Normal Flow
+          </Link>
+        </li>
+        <li>
+          <Link href="/display" className="hover:underline">
+            display inline block
+          </Link>
+        </li>
+        <li>
+          <Link href="/flex-direction-wrap-order" className="hover:underline">
+            flex-direction, flex-wrap, order
+          </Link>
+        </li>
+        <li>
+          <Link href="/flex-grow-shrink" className="hover:underline">
+            flex-grow, flex-shrink, flex-basis
+          </Link>
+        </li>
+        <li>
+          <Link href="/flex-alignment" className="hover:underline">
+            flex: justify-content, justify-items, align-contents
+          </Link>
+        </li>
+        <li>
+          <Link href="/grid" className="hover:underline">
+            Grid
+          </Link>
+        </li>
+      </ol>
     </div>
   );
 }
