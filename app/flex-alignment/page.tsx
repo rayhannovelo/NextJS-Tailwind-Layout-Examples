3d7bf68bedbc3a48ac0da81c308676f245d6b506
Muhammad Rayhan Novelo <rayhan.novelo@gmail.com>
2024-06-24 09:50:31 +0700

    add align-content, gap example

--- app/flex-alignment/page.tsx
+++ app/flex-alignment/page.tsx
@@ -11,47 +11,34 @@ export default function Page() {
         <h3>Flex Notes</h3>
         <ol className="list-disc pl-5">
           <li>justify-content {"->"} justify-center: x-axis</li>
-          <li>align-items {"->"} items-center: y-axis</li>
+          <li>align-items {"->"} items-center: y-axis on row</li>
+          <li>align-content {"->"} content-start: y-axis on all content</li>
+          <li>use gap instead margin</li>
         </ol>
       </div>
 
       <hr className="my-5"></hr>
 
-      <h3>display: flex, justify-center, items-center</h3>
-      <div className="flex flex-wrap justify-center items-center bg-red-400 min-h-[450px] p-5">
+      <h3>display: flex, justify-center, items-end, content-center, gap-5</h3>
+      <div className="flex flex-wrap justify-center items-end content-center gap-5 bg-red-400 h-[1200px] p-5">
         <div className="content bg-blue-400 flex flex-col justify-center items-center text-center">
           <h1>Content 1</h1>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
-            asperiores provident? Porro enim praesentium itaque. Eveniet enim
-            amet recusandae cupiditate! Dolor facere ex quis consequatur amet,
-            eum maiores error fugit! lor
-          </p>
         </div>
-        <div className="content bg-emerald-400 flex flex-col justify-center items-center text-center">
+        <div className="content bg-emerald-400 h-[500px] flex flex-col justify-center items-center text-center">
           <h1>Content 2</h1>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
-            asperiores provident? Porro enim praesentium itaque. Eveniet enim
-            amet recusandae cupiditate! Dolor facere ex quis consequatur amet,
-            eum maiores error fugit!
-          </p>
         </div>
         <div className="content bg-yellow-400 flex flex-col justify-center items-center text-center">
           <h1>Content 3</h1>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
-            asperiores provident? Porro enim praesentium itaque. Eveniet enim
-            amet recusandae cupiditate! Dolor facere ex quis consequatur amet,
-            eum maiores error fugit!
-          </p>
+        </div>
+        <div className="content bg-fuchsia-400 flex flex-col justify-center items-center text-center">
+          <h1>Content 4</h1>
         </div>
       </div>
 
       <hr className="my-5"></hr>
 
       <h3>display: flex, justify-end, items-start</h3>
-      <div className="flex flex-wrap justify-end items-start bg-red-400 min-h-[450px] p-5">
+      <div className="flex flex-wrap justify-end items-start bg-red-400 min-h-[600px] p-5">
         <div className="content bg-blue-400 flex flex-col justify-center items-center text-center">
           <h1>Content 1</h1>
           <p>
@@ -84,7 +71,7 @@ export default function Page() {
       <hr className="my-5"></hr>
 
       <h3>display: flex, justify-start, items-end</h3>
-      <div className="flex flex-wrap justify-start items-end bg-red-400 min-h-[450px] p-5">
+      <div className="flex flex-wrap justify-start items-end bg-red-400 min-h-[600px] p-5">
         <div className="content bg-blue-400 flex flex-col justify-center items-center text-center">
           <h1>Content 1</h1>
           <p>
--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
         </li>
         <li>
           <Link href="/flex-alignment" className="hover:underline">
-            flex: justify-content, justify-items, align-contents
+            flex: justify-content, justify-items, align-contents, gap
           </Link>
         </li>
         <li>
